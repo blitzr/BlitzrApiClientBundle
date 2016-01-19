68ba89b835f7bc614becb31065fbafece70e260d
Martin Le Guillou <martin.leguillou@y-nov.com>
2016-01-19 15:33:40 +0100

    getArtist

--- Service/BlitzrApiService.php
+++ Service/BlitzrApiService.php
@@ -2,17 +2,27 @@
 
 namespace Blitzr\ApiClientBundle\Service;
 
+use Blitzr\BlitzrClient;
+
 class BlitzrApiService
 {
     private $apiKey;
+    private $client;
 
     public function __construct($apiKey)
     {
         $this->apiKey = $apiKey;
+        $this->client = new BlitzrClient($apiKey);
     }
 
     public function getApiKey()
     {
         return $this->apiKey;
     }
+
+    public function getArtist($slug = null, $uuid = null, $extras = [], $extras_limit = null)
+    {
+        return $this->client->getArtist($slug, $uuid, $extras, $extras_limit);
+    }
+
 }
